d211a365947d90fb77071d4d530f35cd330ae722
MGallo-Code <mgallo.code@gmail.com>
2026-03-08 03:40:48 -0400

    Update ResumePreview to use new terminology

--- app/components/ResumePreview.tsx
+++ app/components/ResumePreview.tsx
@@ -1,12 +1,39 @@
-import { Properties } from "csstype";
-import { sampleResume } from "../sample-resume";
+"use client";
+import { sampleResume } from "../fixtures";
+import { Block, Content } from "../models";
+import { JSX } from "react";
 
 export default function ResumePreview() {
-  const redTestStyle: Properties = {
-    backgroundColor: "red",
-    width: "100%",
-    minHeight: "100vh",
-    flexGrow: "1",
+  const resume = sampleResume;
+
+  const blockContent = (content: Content) => {
+    return <p>{content.type}</p>;
   };
-  return <main style={redTestStyle}>{sampleResume.template.name}</main>;
+
+  const regionBlocks = (blocks: Record<string, Block>, blockIds: string[]) => {
+    return blockIds.map((blockId) => {
+      const block = blocks[blockId];
+      let header: JSX.Element | null = null;
+      if (block.header) {
+        const Tag = `h${block.header.level}` as keyof JSX.IntrinsicElements;
+        header = <Tag>{block.header.content.map((r) => r.text).join("")}</Tag>;
+      }
+      return (
+        <div key={blockId} style={block.style}>
+          {header}
+          {blockContent(block.content)}
+        </div>
+      );
+    });
+  };
+
+  return (
+    <main style={{ display: "grid", gridTemplate: resume.template.grid }}>
+      {Object.entries(resume.blockIdsByRegion).map(([regionId, blockIds]) => (
+        <div key={regionId} style={resume.template.areas[regionId].style}>
+          {regionBlocks(resume.blocks, blockIds)}
+        </div>
+      ))}
+    </main>
+  );
 }

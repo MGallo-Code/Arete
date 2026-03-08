dc76dfc39e6006e22b837d0cac1b14ed63327917
MGallo-Code <mgallo.code@gmail.com>
2026-03-07 19:45:26 -0500

    Remove boilerplate code--just seems to be adding too much unnecessary clutter to the page style

--- app/components/ResumePreview.tsx
+++ app/components/ResumePreview.tsx
@@ -1,6 +1,12 @@
+import { Properties } from "csstype";
 import { sampleResume } from "../sample-resume";
 
 export default function ResumePreview() {
-  console.log(sampleResume);
-  return <>{sampleResume.template.name}</>;
+  const redTestStyle: Properties = {
+    backgroundColor: "red",
+    width: "100%",
+    minHeight: "100vh",
+    flexGrow: "1",
+  };
+  return <main style={redTestStyle}>{sampleResume.template.name}</main>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import ResumePreview from "./components/ResumePreview";
-import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <ResumePreview />
-      </main>
-    </div>
-  );
+  return <ResumePreview />;
 }

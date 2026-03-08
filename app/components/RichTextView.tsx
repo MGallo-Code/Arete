bedc7c96a34c5f1a1f67291a5d6d1f64d602005f
MGallo-Code <mgallo.code@gmail.com>
2026-03-08 04:35:06 -0400

    Finish component for rendering RichText (list of differently-styled text runs)

--- app/components/RichTextView.tsx
+++ app/components/RichTextView.tsx
@@ -1,9 +1,19 @@
-import { RichText } from "../models";
+import type { RichText } from "../models";
 
 interface Props {
   richText: RichText;
 }
 
 export default function RichTextView({ richText }: Props) {
-  return <></>;
+  return richText.map((run, index) => {
+    return run.link ? (
+      <a key={index} href={run.link} style={run.style}>
+        {run.text}
+      </a>
+    ) : (
+      <span key={index} style={run.style}>
+        {run.text}
+      </span>
+    );
+  });
 }
